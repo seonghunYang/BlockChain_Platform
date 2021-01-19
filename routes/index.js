d4c9c99c2375981386ffd4c7f8a8d5638e43acdc
Dongjun Jeong <dkdlel1087@naver.com>
2021-01-19 17:49:58 +0900

    거의 완성

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,47 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "main" });
+  res.render("main", { title: "main" });
 });
 
 router.get("/progressLayout", function (req, res, next) {
   res.render("progress/progressLayout", { title: "progressLayout" });
 });
 
-router.get("/registerLayout", function (req, res, next) {
-  res.render("registerLayout", { title: "register", seller: {} });
+router.get("/register", function (req, res, next) {
+  res.render("register", { title: "register", seller: {} });
 });
 
-router.get("/authLayout", function (req, res, next) {
-  res.render("authLayout", { title: req.query.title });
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: req.query.title });
 });
 
-router.get("/storeLayout", function (req, res, next) {
-  res.render("storeLayout");
+router.get("/join", function (req, res, next) {
+  res.render("join", { title: req.query.title });
 });
 
-router.get("/authLayout", function (req, res, next) {
-  res.render("authLayout", { prodctTitle: req.query.title });
-});
-
-router.get("/authLayout", function (req, res, next) {
-  res.render("authLayout", { productExplain: req.query.title });
+router.get("/store", function (req, res, next) {
+  res.render("store");
 });
 
 router.get("/category", function (req, res, next) {
   res.render("category", { title: req.query.title });
+});
+
+router.get("/registerService", function (req, res, next) {
+  res.render("registerService");
+});
+
+router.get("/serviceInfo", function (req, res, next) {
+  res.render("serviceInfo");
+});
+
+router.get("/serviceInfoEditor", function (req, res, next) {
+  res.render("serviceInfoEditor");
+});
+
+router.get("/completeTransaction", function (req, res, next) {
+  res.render("completeTransaction");
 });
 
 module.exports = router;
